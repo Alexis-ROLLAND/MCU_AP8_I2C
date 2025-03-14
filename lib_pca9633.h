536e6b0da18c4b5c7b8242a52bf59e412d0b07ba
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-03-14 15:01:32 +0100

    OK for 2024-2025

--- lib_pca9633.h
+++ lib_pca9633.h
@@ -208,7 +208,7 @@ typedef enum {PCA9633_DIMMING_GROUP_CONTROL, PCA9633_BLINKING_GROUP_CONTROL} pca
  * @Attention Low Level Function - Should not be used by app 
  *
  */
- pca9633_err_t pca9633_write_reg(const pca9633_desc_t *pPca9633,uint8_t RegAddr, uint8_t RegValue);
+ extern pca9633_err_t pca9633_write_reg(const pca9633_desc_t *pPca9633,uint8_t RegAddr, uint8_t RegValue);
  
  /**
  * @brief       Reads a byte from a PCA9633 register   
@@ -223,7 +223,7 @@ typedef enum {PCA9633_DIMMING_GROUP_CONTROL, PCA9633_BLINKING_GROUP_CONTROL} pca
  * @Attention Low Level Function - Should not be used by app 
  *
  */
- pca9633_err_t pca9633_read_reg(const pca9633_desc_t *pPca9633,uint8_t RegAddr, uint8_t *pRegValue);
+ extern pca9633_err_t pca9633_read_reg(const pca9633_desc_t *pPca9633,uint8_t RegAddr, uint8_t *pRegValue);
  
  
          
